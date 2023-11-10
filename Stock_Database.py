3a588bc802d5b5b9b22639aace4f6275992025ae
gavinmcdavitt <80462428+gavinmcdavitt@users.noreply.github.com>
2023-11-10 11:43:38 -0800

    Update Stock_Database.py

--- Stock_Database.py
+++ Stock_Database.py
@@ -1,57 +1,83 @@
 import sqlite3
 import stockprices
-
+import time
+import datetime
+import threading
+# this is great for keeping an up to date stock database with results from a previous time
+conn = sqlite3.connect('new_stock_2.db', check_same_thread=False, isolation_level=None)
+c = conn.cursor()
+'''''''''''
+#TESTING CODE DO NOT ADD TO DATABASE
 # Connect to the in-memory database, this is great for testing as it does not include only database values
 #conn = sqlite3.connect(':memory:')
-
-#this is great for keeping an up to date stock database with results from a previous time
-conn = sqlite3.connect('stock.db')
-
-c = conn.cursor()
-
 # Define the SQL query to create the "stocks" table this will only be used for RAM memory storage
-# create_table_query = """
-# CREATE TABLE stocks (
-#  symbol TEXT,
-#  price REAL,
-#  changeInDollars REAL,
-#  changeInPercent REAL
-# )
-# """
 
-# Execute the create table query for only if using RAM
-#c.execute(create_table_query)
-
+# 
+# # Execute the create table query for only if using RAM
+'''''''''''
+create_table_query = """
+CREATE TABLE IF NOT EXISTS stocks (
+symbol TEXT,
+price REAL,
+changeInDollars REAL,
+changeInPercent REAL,
+openat REAL,
+peratio REAL,
+marketcap REAL,
+dailyrange REAL
+)
+ """
+c.execute(create_table_query)
 # Define the symbol
-for s in stockprices.MyStocks:
- price = stockprices.getPrice(s)
- Cdollar = stockprices.getChangeInDollars(s)
- Cpercent = stockprices.getChangeInPercent(s)
+def StockInfo():
+    for s in stockprices.MyStocks:
+        price = stockprices.getPrice(s)
+        Cdollar = stockprices.getChangeInDollars(s)
+        Cpercent = stockprices.getChangeInPercent(s)
+        openAt = stockprices.getOpenAt(s)
+        PERatio = stockprices.getPERatio(s)
+        marketCap = stockprices.getMarketCap(s)
+        dailyR = stockprices.getDailyRange(s)
 
- # Define the SQL query with placeholders for inserting data
- insert_query = "INSERT INTO stocks (symbol, price, changeInDollars, changeInPercent) VALUES (?,?,?,?)"
+        # Define the SQL query with placeholders for inserting data
+        insert_query = "INSERT INTO stocks (symbol, price, changeInDollars, changeInPercent, openat, peratio, marketcap, dailyrange) " \
+                       "VALUES (?,?,?,?,?,?,?,?)"
 
- # Execute the query to insert the data for the first stock
- with conn:
-     c.execute(insert_query, (s, price, Cdollar, Cpercent))
+        # Execute the query to insert the data for the first stock
+        with conn:
+            c.execute(insert_query, (s, price, Cdollar, Cpercent, openAt, PERatio, marketCap, dailyR))
 
-# Commit the changes to the database
-conn.commit()
+    # Commit the changes to the database
+    conn.commit()
 
-# Query the database to retrieve the data for a specified symbol
-#c.execute("SELECT * FROM stocks WHERE symbol = ?", (a,))
+    # Query the database to retrieve the data for a specified symbol
+    # c.execute("SELECT * FROM stocks WHERE symbol = ?", (a,))
 
-#query from all tables in database!
-c.execute("SELECT * FROM stocks")
-
-result = c.fetchall()
-print(result)
-
+    # query from all tables in database!
+    c.execute("SELECT * FROM stocks")
 
 def delete_entry():
     with conn:
-        c.execute("DELETE * FROM stocks WHERE symbol=:symbol", {'symbol': 's'})
+        c.execute("DELETE FROM stocks WHERE symbol=:symbol", {'symbol': 's'})
+
+
 # Close the connection
 
+def automate_scraping():
+    while True:
+        # Get the current date and time
+        now = datetime.datetime.now()
 
-conn.close()
+        # Check if it's a weekday (Monday to Friday)
+        if now.weekday() < 5:
+            # Check if the current time is between 10:00 AM and 4:00 PM Eastern Standard Time
+            if datetime.time(10, 0) <= now.time() <= datetime.time(16, 0):
+                StockInfo()
+                print("automating")
+                res = c.fetchall()
+                print(res)
+            else:
+                print("sleeping")
+        # Sleep for 900 seconds (15 minutes) before the next iteration
+        time.sleep(300)
+
